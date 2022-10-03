64fa0fbd04d09e2da073d6a110d86ae5ba49de31
belino93 <abelmadrid93@gmail.com>
2022-10-03 16:53:23 +0200

    reto1 terminado

---
+++ main.js
@@ -0,0 +1,14 @@
+/* Declara 2 variables numéricas (con el valor que desees), indica cual es mayor de
+los dos. Si son iguales indicalo también. Puedes ir cambiando los valores
+para comprobar que funciona. */
+
+let num1 = 4;
+let num2 = 6;
+
+if (num1 > num2) {
+    console.log(num1 + ' es mayor que ' + num2);
+}else if(num1 < num2){
+    console.log(num1 + ' es menor que ' + num2);
+}else{
+    console.log('Los dos números son iguales');
+}
